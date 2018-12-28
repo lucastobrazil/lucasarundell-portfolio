0bfe40b70921ddf3544e003c2e95a781e3ffb16c
lucastobrazil <lucas.arundell@gmail.com>
2018-12-28 22:54:36 +1000

    basic set up of next

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const withLess = require('@zeit/next-less');
+module.exports = withLess({
+    cssModules: true,
+    cssLoaderOptions: {
+        importLoaders: 1,
+        modules: true,
+        localIdentName: '[folder]-[local]-[hash:base64:5]',
+    },
+});
