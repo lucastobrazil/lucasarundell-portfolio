5bf6feb1957f9eac953485738cd6f56f1b17e537
lucastobrazil <lucas.arundell@gmail.com>
2018-12-29 00:19:26 +1000

    add SEO tags etc

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -4,12 +4,17 @@ import Nav from '../Nav';
 import Footer from '../Footer';
 import '../../styles/app.less';
 
-export default ({ children, title = 'Lucas Arundell' }) => (
+export default ({ children, title = 'Lucas Arundell', description = 'A Berlin-based Product Designer.' }) => (
     <div>
         <Head>
             <title>{title}</title>
             <meta charSet="utf-8" />
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <meta name="description" content={description} />
+            <link rel="shortcut icon" href="/static/icons/favicon-me.ico" />
+            <meta property="og:title" content={title} />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="http://www.lucasarundell.com" />
         </Head>
         <Nav />
         <main>{children}</main>
--- pages/content/index.js
+++ pages/content/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import WorkItemDetail from '../../components/WorkItemDetail';
 import WorkItem from '../../components/WorkItem';
 
 import CONTENT from './content';
 
-const ArundelPage = () => <WorkItemDetail {...CONTENT.ADL} />;
-const CddjPage = () => <WorkItemDetail {...CONTENT.CDDJ} />;
-const HhrPage = () => <WorkItemDetail {...CONTENT.HHR} />;
-const MmnPage = () => <WorkItemDetail {...CONTENT.MMN} />;
-const NmPage = () => <WorkItemDetail {...CONTENT.NM} />;
-const SiPage = () => <WorkItemDetail {...CONTENT.SI} />;
-
-export { ArundelPage, CddjPage, HhrPage, MmnPage, NmPage, SiPage };
 
 export const workItems = [
     <WorkItem {...CONTENT.SI} />,
